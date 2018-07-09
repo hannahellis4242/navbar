679d34dadc2b348ff0a76b4eae8d9b538bc3c4c1
Hannah <hannahellis4242@gmail.com>
2018-07-09 22:21:22 +0000

    make sure that routes can find pageData.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var pageData = require('pageData');
+var pageData = require('../pageData');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var pageData = require('pageData');
+var pageData = require('../pageData');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
